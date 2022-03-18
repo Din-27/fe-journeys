80ba72f9744f2bb40750a5534cae5f93cfd8544f
unknown <Herdiyana-gif>
2022-03-18 10:55:49 +0700

    update

--- src/config/api.js
+++ src/config/api.js
@@ -1,24 +1,29 @@
+import axios from "axios";
 
 // Create base URL API
-export const API = () => {
-  const baseUrl =
-    process.env.REACT_APP_SERVER_URL ||
-    'https://journey-app27.herokuapp.com/api/v1' ||
-    'https://localhost:5000/api/v1';
+export const API = axios.create({
+  baseURL: process.env.REACT_APP_SERVER_URL ||
+  "https://journey-app27.herokuapp.com/api/v1",
+});
 
-  const executeAPI = async (endpoint, config) => {
-    const response = await fetch(baseUrl + endpoint, config);
-    const data = await response.json();
-    return data;
-  };
+// const baseUrl =
+//     process.env.REACT_APP_SERVER_URL ||
+//     'https://journey-app27.herokuapp.com/api/v1' ||
+//     'https://localhost:5000/api/v1';
 
-  return {
-    get: executeAPI,
-    post: executeAPI,
-    patch: executeAPI,
-    delete: executeAPI,
-  };
-};
+//   const executeAPI = async (endpoint, config) => {
+//     const response = await fetch(baseUrl + endpoint, config);
+//     const data = await response.json();
+//     return data;
+//   };
+
+//   return {
+//     get: executeAPI,
+//     post: executeAPI,
+//     patch: executeAPI,
+//     delete: executeAPI,
+//   };
+
 // Set Authorization Token Header
 export const setAuthToken = (token) => {
   if (token) {
